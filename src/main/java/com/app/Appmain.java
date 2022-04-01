3d30cc4ad58ff093571d7266c4168b17f948cd73
skjavab <srshryls@gamil.com>
2022-04-01 11:53:41 +0530

    Creating initial project structure and add dependency

---
+++ src/main/java/com/app/Appmain.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appmain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Appmain.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/AppmainTests.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
